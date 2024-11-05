27999b66d70c14b2d4457794715639a4fbeef76a
010 <010@gmail.com>
2024-11-05 10:54:11 +0800

    feat: update

--- setup.py
+++ setup.py
@@ -37,5 +37,9 @@ setuptools.setup(
         'Operating System :: OS Independent'
     ],
     cmdclass={'build_ext': torch.utils.cpp_extension.BuildExtension},
-    packages=setuptools.find_packages()
+    packages=setuptools.find_packages(),
+    package_data={
+        'damo': ['*.txt']  # 指定包含 damo 包中的所有 .txt 文件
+    },
+    include_package_data=True,  # 确保包含包内的额外文件
 )
